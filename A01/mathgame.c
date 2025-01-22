0600b1e105a753e06375915a64b35c1f22f0930b
Olivia Boyer <oliviaboyer@wc126-dhcp-93.brynmawr.edu>
2025-01-22 12:27:37 -0500

    finished except valgrind

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,16 +1,33 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Olivia Boyer
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int rounds, userAnswer, correctAnswer, correctNum;
+    int rounds, userAnswer, correctAnswer;
+    int correctNum = 0;
+    int numOne, numTwo;
     printf("Welcome to Math Game!\n");
-    printf("How many rounds do you want to play?");
+    printf("How many rounds do you want to play? ");
     scanf("%d", &rounds);
-        
+    for (int i = 0; i < rounds; i++) {
+            numOne = (rand() % 9) + 1;
+            numTwo = (rand() % 9) + 1;
+            correctAnswer = numOne + numTwo;
+            printf("%d + %d = ? ", numOne, numTwo);
+            scanf("%d", &userAnswer);
+            if (userAnswer == correctAnswer) {
+                printf("Correct!\n");
+                correctNum += 1;
+            } else {
+                printf("Incorrect :(\n");
+            }
+    }
+    
+    printf("You answered %d/%d correctly.\n", correctNum, rounds);
     return 0;
 }
