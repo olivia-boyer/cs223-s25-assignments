e5e5c0cdba51dc9f3ed872ab604bac5bba064c67
Olivia Brown Boyer <oboyer@goldengate.cs.brynmawr.edu>
2025-01-23 23:03:44 -0500

    add whitespace

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -8,23 +8,36 @@
 #include <stdlib.h>
 
 int main() {
+
     int rounds, userAnswer, correctAnswer;
     int correctNum = 0;
     int numOne, numTwo;
+
     printf("Welcome to Math Game!\n");
     printf("How many rounds do you want to play? ");
+
     scanf("%d", &rounds);
+
     for (int i = 0; i < rounds; i++) {
+
             numOne = (rand() % 9) + 1;
             numTwo = (rand() % 9) + 1;
+
             correctAnswer = numOne + numTwo;
+
             printf("\n%d + %d = ? ", numOne, numTwo);
+
             scanf("%d", &userAnswer);
+
             if (userAnswer == correctAnswer) {
                 printf("Correct!\n");
+
                 correctNum += 1;
+
             } else {
+
                 printf("Incorrect :(\n");
+
             }
     }
     
