b536c809c500ec18240e929099007edd67333ccf
Olivia Brown Boyer <oboyer@goldengate.cs.brynmawr.edu>
2025-01-30 22:37:54 -0500

    assignment 2 complete

--- A02/cypher.c
+++ A02/cypher.c
@@ -1,9 +1,31 @@
 /***************************************************
- * Author: 
- * 
+ * Author: Olivia Boyer 
+ * asks user for a word at prints a shift cypher of 
+ * it using a shift number given by user.
  */
 #include <stdio.h>
+#include <string.h>
+
+void cypherfy(char input[], int shift) {
+    int length = strlen(input);
+    for (int i = 0; i < length; i++) {
+        input[i] = (((input[i] % 97) + shift + 26) % 26) + 97;
+      
+    }
+}
 
 int main() {
-  return 0;
+    int shift;
+    char word[100];
+    
+    printf("Enter a word: ");
+    scanf("%s", word);
+    
+    printf("\nEnter a shift: ");
+    scanf("%d", &shift);
+    
+    cypherfy(word, shift);  
+    
+    printf("Your cypher is %s\n", word); 
+    return 0;
 }
--- A02/password.c
+++ A02/password.c
@@ -1,9 +1,30 @@
 /***************************************************
- * Author: 
- * 
+ * Author: Olivia Boyer 
+ * creates a bad password from inputted word.
  */
 #include <stdio.h>
+#include <string.h>
+
+void makePassword(char word[]) {
+    for (int i = 0; i < strlen(word); i++) {
+        if (word[i] == 'e') {
+            word[i] = '3';
+        } else if (word[i] == 'l') {
+            word[i] = '1';
+        } else if (word[i] == 'a') {
+            word[i] = '@';
+        }
+        }
+    return;
+}   
+        
 
 int main() {
-  return 0;
+
+    char word[100];
+    printf("Enter a word: ");
+    scanf("%s", word);
+    makePassword(word);
+    printf("\nYour bad password is %s\n", word);    
+    return 0;
 }
