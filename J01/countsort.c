7e4354cc8cb5259de07f22468613a04cbabbc10e
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-02-04 16:58:28 -0500

    finished lab

--- J01/countsort.c
+++ J01/countsort.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-   return 0;
-}
+    printf("Enter digits: ");
+    char* c_arr = malloc(sizeof(char) * 32);
+    scanf("%s", c_arr);
+    int* i_arr = malloc(sizeof(int) *96);
+    int idx = 0; 
+    for (int i = 0; i < strlen(c_arr); i++){
+        if (c_arr[i] < 58 && c_arr[i] > 47) {
+           i_arr[idx] = c_arr[i] - 48;   
+           idx += 1;
+        } else {
+           if (c_arr[i] > 100) {
+              i_arr[idx] = 1;
+              idx +=1;
+              c_arr[i] -= 100;
+        }            
+        i_arr[idx] = c_arr[i] / 10;
+        idx += 1;
+        i_arr[idx] = c_arr[i] % 10;
+        idx += 1;
+        }
+    }
+    
+    free(c_arr);
+    int temp, swapped;
+    for (int i = 0; i < idx; i++) {
+       swapped = 0;
+       for (int j = 0; j  < idx - i - 1; j++) {
+           if (i_arr[j] > i_arr[j + 1]) {
+               temp = i_arr[j];
+               i_arr[j] = i_arr[j + 1]; 
+               i_arr[j + 1] = temp;
+               swapped = 1;
+           }    
+       }
+       if (swapped == 0){
+           break;
+       }
+    }
+    for (int i = 0; i < idx; i++) {
+        printf("%d ", i_arr[i]);
+    } 
+    printf("\n");
+    free(i_arr);
+    return 0;
+}    
+       
+    
--- J01/price.c
+++ J01/price.c
@@ -1,7 +1,73 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main()
-{
-   return 0;
+int main() {
+    srand(time(NULL));
+    int price = rand() % (5000 - 3000 + 1) + 3000;
+    int youGuess;
+    printf("Welcome to the Price is Right! \n");
+    printf("Enter a guess to win a luxurious dishwasher: $");
+    scanf("%d", &youGuess);
+    int AI_1 = rand() % (5000 - 3000 + 1) + 3000;
+    int AI_2 = rand() % (5000 - 3000 + 1) + 3000;
+    printf("AI contestant #1 guesses %d\n", AI_1);
+    printf("AI contestant #2 guesses %d\n", AI_2);
+    printf("The dishwasher costs %d\n", price);
+    if (price < AI_1 && price < AI_2 && price < youGuess){
+        printf("Sorry, nobody wins the dishwasher. I'm keeping it.");
+    } else {
+        if (AI_1 > AI_2) {
+             if (AI_1 > youGuess) {
+                 if(AI_1 < price) {
+                     printf("AI contestant #1 wins the dishwasher");
+                 }
+                 else {
+                 if (AI_2 > youGuess) {
+                     if (AI_2 < price) {
+                        printf("AI contestant #2 wins the dishwasher");
+                     }
+                    else {
+                        printf("You win the dishwasher!");
+                    }
+                }
+                else {
+                    if (youGuess < price) {
+                        printf("You win the dishwasher!");
+                    }
+                    else {
+                        if (AI_1 < price) {
+                             printf("AI contestant #1 wins the dishwasher");
+                        }
+                       printf("AI contestant #2 wins the dishwasher");
+                    }
+                }
+             }
+            }
+        }
+        else if (AI_2 > youGuess) {
+            if (AI_2 < price) {
+                printf("AI Contestant 2 wins the dishwasher");
+               }
+            else if (youGuess < price){
+                printf("You win the dishwasher");
+            }
+            else {
+                printf("AI contestant #1 wins the dishwasher");
+            }
+        }
+            else if (youGuess < price) {
+                printf("You win the dishwasher!");
+            }
+        else {
+            if (AI_2 < price) {
+              printf("AI Contestant #2 wins the dishwasher!");
+            }
+            else {
+            printf("AI contestant #1 wins the dishwasher");
+            }
+        }   
+    return 0;
+    }
 }
 
