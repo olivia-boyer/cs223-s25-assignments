94c7db2674621bc833bda057f7261b6a623725c7
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-03-05 21:10:29 -0500

    finished J03

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -1,4 +1,88 @@
-int main()
+//Olivia and Maryam 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MASK 0xff
+
+int hex_to_int(const char *val)
 {
+  int value=0;
+  int i = 2;
+
+  while (val[i] != '\0')
+  {
+    if (val[i] >= '0' && val[i] <= '9')
+    {
+      value = (value << 4) | (val[i] - '0');
+    }
+    else if (val[i] >= 'A' && val[i] <= 'F')
+    {
+      value = (value << 4) | (val[i] - 'A' + 10);
+    }
+    else
+    {
+      printf("invalid number");
+      exit(1);
+    }
+    i++;
+  }
+
+  return value;
+}
+
+int bin2int(char *val)
+{
+  int value=0;
+  int i = 2;
+
+  while (val[i] != '\0')
+  {
+    if (val[i] == '0' || val[i] == '1')
+    {
+      value = (value << 1) | (val[i] - '0');
+
+    }
+    else
+    {
+      printf("invalid number\n");
+      exit(1);
+    }
+    i++;
+
+  }
+  return value;
+}
+int main(int argc, char **argv)
+{
+
+  if (argc != 2)
+  {
+    printf("Error: please enter a value");
+    exit(1);
+  }
+
+  char *v = argv[1];
+
+  if (argv[1][0] == '0')
+  {
+    if (argv[1][1] == 'b')
+    {
+      printf("%d\n", bin2int(argv[1])) ;
+    }
+    else if (argv[1][1] == 'x')
+    {
+      printf("%d\n", hex_to_int(argv[1]));
+    }
+    else
+    {
+      printf("Error: please enter a value\n");
+      exit(1);
+    }
+  }
+
+
   return 0;
 }
+
--- J03/int2hex.c
+++ J03/int2hex.c
@@ -1,4 +1,74 @@
-int main()
+// Olivia and Maryam
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MASK 0x80000000 // 32 bits
+
+#define hexMASK 0xF0000000 // last 4 bits converted to hex
+
+void binVal(int num)
 {
+  printf("0b ");
+  for (int i = 0; i < 8; i++)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      if ((MASK & num) == MASK)
+      {
+        printf("1");
+      }
+      else
+      {
+        printf("0");
+      }
+
+      num = num << 1;
+    }
+    printf(" ");
+  }
+  printf("\n");
+}
+
+void bitVal(int num)
+{
+  printf("0x ");
+  unsigned int temp;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int i = 0; i < 2; i++)
+    {
+      temp = hexMASK & num;
+      temp = temp >> 28;
+
+      if (temp < 10)
+      {
+        printf("%d", temp);
+      }
+      else
+      {
+        printf("%c", 'A' + (temp - 10));
+      }
+      num = num << 4;
+    }
+    printf(" "); 
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv)
+{
+
+  if (argc == 1)
+  {
+    printf("Error: please enter a value\n");
+    exit(1);
+  }
+
+  int v = atoi(argv[1]);
+
+  bitVal(v);
+  binVal(v);
+
   return 0;
 }
