fa1e4610585a7da815717d4110105eafd911c4ef
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-03-07 16:49:36 -0500

    part 1

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -4,12 +4,31 @@
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MASK 0x1ul << 63
+
+void print8x8(unsigned long input) {
+    
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if ((input & MASK) == MASK) {
+                printf("@ ");
+            } else { 
+                printf("  ");
+            }
+            input = input << 1;
+        }
+        printf("\n");
+    }       
+}
 
 int main() {
-  unsigned long img;
-  scanf(" %lx", &img);
-  printf("Image (unsigned long): %lx\n", img);
-
-  // todo: your code here
-  return 0;
+    unsigned long img;
+    scanf(" %lx", &img);
+    printf("Image (unsigned long): %lx\n", img);
+    
+    print8x8(img);
+    return 0;
 }
--- A07/read_ppm.c
+++ A07/read_ppm.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Olivia Boyer 
+ * Date: 02/19/25
+ * Description: Reads in a ppm file and stores pixel data in an array. 
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,43 @@
 // Choose *one* to implement (do not remove the other one!)
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+
+    FILE* file;
+    struct ppm_pixel* pArray;
+    
+    file = fopen(filename, "rb");
+
+    if (file == NULL) {
+        printf("Error opening file");
+        return NULL;
+    } 
+
+    if (fgetc(file) != 'P') {
+        if (fgetc(file) != '6') {
+            printf("wrong filetype");
+            return NULL;
+        }
+    }
+
+    fseek(file, 2, SEEK_CUR); 
+
+    while (fgetc(file) != '\n') {
+     
+    }
+
+    int range;
+    fscanf(file, "%d %d\n %d\n", w, h, &range);
+    pArray = (struct ppm_pixel*)malloc(sizeof(struct ppm_pixel*) * (*w) * (*h));
+
+    if (pArray == NULL) {
+        printf("Error creating array");
+        return NULL;
+    }   
+
+    fread(pArray, sizeof(struct ppm_pixel), *w * *h, file);     
+    fclose(file);    
+       
+    return pArray;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
