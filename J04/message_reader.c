600cd7c1a55836aeced4f75f9a00013a5a558828
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-03-25 16:32:06 -0400

    finished J04

--- J04/message_reader.c
+++ J04/message_reader.c
@@ -20,19 +20,22 @@ void readbinfile(const char* filename){
       return;
   }
   
-char comment[256];
-  fgets(comment,255,file);
-struct metadata* store = malloc(sizeof(struct metadata));
-fread((&store->version), sizeof(float),1, file );
-fread((&store->year), sizeof(unsigned int), 1, file);
-fread((&store->length), sizeof(unsigned int),1, file);
-char* message = malloc(sizeof(char) * store->length); 
+    char comment[256];
+    fgets(comment,255,file);
+    struct metadata* store = malloc(sizeof(struct metadata));
+    fread((&store->version), sizeof(float),1, file );
+    fread((&store->year), sizeof(unsigned int), 1, file);
+    fread((&store->length), sizeof(unsigned int),1, file);
+    char* message = malloc(sizeof(char) * store->length); 
 fread(message, sizeof(char), store->length, file);
 printf("comment :%s \n",comment);
 printf("version :%f \n", store->version);
 printf("year : %d \n", store->year);
 printf("length : %d \n", store->length);
 printf("message : %s \n", message);
+free(store);
+free(message);
+fclose(file);
 }
 int main(int argc, char** argv){
 readbinfile (argv[1]);
--- J04/message_writer.c
+++ J04/message_writer.c
@@ -1,4 +1,33 @@
-int main()
-{
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct metadata {
+
+float version;
+unsigned int year;
+unsigned int length;
+
+};
+
+
+void writeBin(const char* filename) {
+    
+    FILE* writeTo = fopen(filename, "wb");
+    const char* comment = "#This is a comment\n";
+    const char* message = "This is the message";
+    struct metadata* meta = malloc(sizeof(struct metadata));
+    meta->version = 1.0;
+    meta->year = 2025;
+    meta->length = 20;
+    fwrite(comment, sizeof(char), strlen(comment), writeTo); 
+    fwrite(meta, sizeof(struct metadata), 1, writeTo);
+    fwrite(message, sizeof(char), meta->length, writeTo);
+    free(meta);
+    fclose(writeTo);
 }
+
+int main(int arg, char** argv) {
+    writeBin(argv[1]);
+}
+     
