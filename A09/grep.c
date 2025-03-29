85ac91b4da2db1f7ba4ff5737cc61f6471f5964d
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-03-29 00:07:12 -0400

    in progress

--- A09/grep.c
+++ A09/grep.c
@@ -1,5 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main() {
-  return 0;
+int  scanFiles (char** files, int numFiles, int startID) {
+    char* line = malloc(sizeof(char) * 81);
+    int occurences = 0;
+
+    printf("Process [%d] searching %d files (%d to %d)\n",
+        getpid(), numFiles, startID, startID + numFiles);
+
+    for (int i = startID; i < startID + numFiles; i++) {
+        FILE* file = fopen(files[i], "r");
+        if (file == NULL) {
+            perror("Error");
+            continue;
+        }
+        while (fgets(line, 81, file)) {
+            char* p = strstr(line, files[2]);
+            if (p) {
+                printf("%d) %s: %s\n", getpid(), files[i], p);
+                occurences += 1;
+            }
+        }
+        fclose(file);
+    }
+     
+    free(line);
+    return occurences;       
+}
+
+int main(int argc, char** argv) {
+    int numProc = atoi(argv[1]);
+    char* keyword = argv[2];
+    int numFiles = argc - 3;
+    int readNum = numFiles / numProc;    
+    int isParent = 1;
+    int startId = 3;
+    int* children = malloc(sizeof(int) * (numProc - 1));
+    
+    printf("searching %d files for keyword: %s \n", numFiles, keyword);
+    
+
+    for (int i = 0; i < numProc - 1; i++) {
+        if (isParent) {
+            isParent = fork();
+            children[i] = isParent;
+        } 
+
+        if (!isParent) {
+            int exitCode = scanFiles(argv, readNum, startId);
+            exit(exitCode);
+        }
+
+        startId += readNum;
+    }
+
+    int temp = 0;
+    int res = 0;
+
+        if (isParent) {
+            readNum = argc - startId;
+            res = scanFiles(argv, readNum, startId);
+
+            for (int i = 0; i < numProc - 1; i++) {
+                waitpid(children[i], &temp, 0);
+                res += WEXITSTATUS(temp);
+                printf("%d %d\n", children[i], WEXITSTATUS(temp));
+            }
+        }
+
+    printf("%d\n", res);
+    free(children);
+    return 0;
 }
--- A09/warmup.c
+++ A09/warmup.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+    
 int main() {
-  return 0;
+    int name = 0xA0;
+    pid_t pid = getpid();
+    printf("%d[ %x\n", pid, name);
+    int isChild = fork();
+    name += 0x10;
+
+    if (isChild == 0) {
+        name += 0x1;
+    }
+
+    pid = getpid(); 
+    printf("%d[ %x\n", pid, name);
+
+    if (isChild) {
+        isChild = fork();        
+        name += 0x10;
+        if (isChild == 0) {
+            name += 0x1;
+        }
+        pid = getpid();
+        printf("%d[ %x\n", pid, name);
+    }
+    printf("Bye\n");
+    return 0;
 }
