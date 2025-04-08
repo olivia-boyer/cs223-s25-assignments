977b356f01c3357bb4f667cc9a36ed985d5dfbe9
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-04-08 15:43:43 -0400

    finished J05

--- J05/dot.c
+++ J05/dot.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
@@ -7,6 +9,30 @@
 #define SIZE 100000
 
 // your code here
+struct thread{
+  pthread_t id;
+  int index;
+  int sum;
+  int v[SIZE];
+  int u[SIZE];
+};
+
+void *dot( void* i){
+  struct thread* threadArr = (struct thread*) i;
+  int* index = &threadArr->index;
+  int dotproduct = 0;
+  int *v = threadArr->v;
+  int *u = threadArr->u;
+  int start = SIZE/4 * *index;
+  int end = SIZE/4 * (*index + 1);
+  for (int i = start; i < end; i++) {
+    dotproduct += u[i] * v[i];
+  }
+
+  threadArr->sum = dotproduct;
+  
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   srand(time(0));
@@ -23,10 +49,28 @@ int main(int argc, char *argv[]) {
   printf("Ground truth dot product: %d\n", dotproduct);
 
   // TODO: Implement your thread solution here
+  
   int thread_dotproduct = 0;
+  
   printf("Test with 4 threads\n");
 
+  struct thread threads[4];
+  
+  for( int i = 0; i < 4; i++){
+    threads[i].index = i;
+    memcpy(threads[i].v, v, sizeof(v));
+    memcpy(threads[i].u, u, sizeof(u));
+    pthread_create(&threads[i].id, NULL, dot, &threads[i]);
+  }
+
+  for( int i = 0; i < 4; i++){
+    pthread_join(threads[i].id, NULL);
+    thread_dotproduct += threads[i].sum;
+  }
+  
   printf("Answer with threads: %d\n", thread_dotproduct);
 
   return 0;
 }
+
+
--- J05/multiply.c
+++ J05/multiply.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
@@ -7,6 +9,35 @@
 #define SIZE 100
 
 // your code here
+struct thread{
+  pthread_t id;
+  int index;
+  int vector[SIZE];
+  int M[SIZE * SIZE];
+  int u[SIZE];
+};
+
+void *threadMultiply( void* i){
+  struct thread* threadArr = (struct thread*) i;
+  int* index = &threadArr->index;
+  int start = SIZE/4 * *index;
+  int end = SIZE/4 * (*index + 1);
+  int *M = threadArr->M;
+  int *u = threadArr->u;
+  int *vector = threadArr->vector;
+
+  for (int i = 0; i < SIZE; i++) {
+    vector[i] = 0;
+  }
+
+  for (int i = start; i < end; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      vector[i] += M[i*SIZE + j] * u[j];
+    }
+  }
+
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   srand(time(0));
@@ -32,7 +63,21 @@ int main(int argc, char *argv[]) {
   // TODO: Implement your thread solution here
   printf("Test with 4 threads\n");
 
+  struct thread threads[4];
+  
+  for( int i = 0; i < 4; i++){
+    threads[i].index = i;
+    memcpy(threads[i].M, M, sizeof(M));
+    memcpy(threads[i].u, u, sizeof(u));
+    pthread_create(&threads[i].id, NULL, threadMultiply, &threads[i]);
+  }
 
+  for( int i =0; i < 4; i++){
+    pthread_join(threads[i].id, NULL);
+    for( int z = SIZE/4 * i; z < SIZE/4 * (i+1); z++){
+      result_threads[z] = threads[i].vector[z];
+    }
+  }
 
   int error = 0;
   for (int i = 0; i < SIZE; i++) {
