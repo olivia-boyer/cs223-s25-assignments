9ba5fa665c4e876225ff6bfa1949809b72d4723f
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-04-11 22:58:09 -0400

    formatting

--- A11/glow.c
+++ A11/glow.c
@@ -1,3 +1,7 @@
+/*Author: Olivia Boyer.
+ * Description: Adds a glow effect to a small ppm image of the earth 
+ * using a box blur effect.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,140 +11,171 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
-int findAverage(struct ppm_pixel* brightMap, int r, int c, int w, int h) {
+int findAverage(struct ppm_pixel *brightMap, int r, int c, int w, int h)
+{
 
-//  printf("%d \n", r);
+  //  printf("%d \n", r);
 
   int highW, lowW, highH, lowH;
   int div = 0;
   int sum = 0;
 
-  if ((w - c) <= 12) {
+  if ((w - c) <= 12)
+  {
     highW = w;
-  } else { 
+  }
+  else
+  {
     highW = c + 12;
   }
 
-  if ((h - r) <= 12) {
+  if ((h - r) <= 12)
+  {
     highH = h;
-  } else { 
+  }
+  else
+  {
     highH = r + 12;
   }
 
-  if ((c) <= 12) {
+  if ((c) <= 12)
+  {
     lowW = 0;
-  } else { 
+  }
+  else
+  {
     lowW = c - 12;
   }
 
-  if ((r) <= 12) {
+  if ((r) <= 12)
+  {
     lowH = 0;
-  } else { 
+  }
+  else
+  {
     lowH = r - 12;
   }
 
-  for (int i = lowH; i < highH; i++) {
-    for (int j = lowW; j < highW; j++) {
+  for (int i = lowH; i < highH; i++)
+  {
+    for (int j = lowW; j < highW; j++)
+    {
       sum += brightMap[(w * i) + j].red;
       div += 1;
     }
   }
-    
-  //printf("%d %d\n", r, c);
-return sum/div;
+
+  // printf("%d %d\n", r, c);
+  return sum / div;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   int w, h;
- 
-  const char* fileName = "earth-small.ppm";
-  struct ppm_pixel* pixels = read_ppm(fileName, &w, &h);
 
-  //printf("%d \n", h);
-  struct ppm_pixel* newPixels = (struct ppm_pixel*) malloc(
-    sizeof(struct ppm_pixel) * w * h);
+  const char *fileName = "earth-small.ppm";
+  struct ppm_pixel *pixels = read_ppm(fileName, &w, &h);
 
-  struct ppm_pixel* blurred = (struct ppm_pixel*) malloc(
-    sizeof(struct ppm_pixel) * w * h); 
+  // printf("%d \n", h);
+  struct ppm_pixel *newPixels = (struct ppm_pixel *)malloc(
+      sizeof(struct ppm_pixel) * w * h);
 
-  if (pixels == NULL || newPixels == NULL || blurred == NULL) {
+  struct ppm_pixel *blurred = (struct ppm_pixel *)malloc(
+      sizeof(struct ppm_pixel) * w * h);
+
+  if (pixels == NULL || newPixels == NULL || blurred == NULL)
+  {
     printf("Error: failed to create array.");
     return 0;
   }
 
   unsigned char brightness = 0;
 
-  for (int i = 0; i < h; i++) {
-    for (int j = 0; j < w; j++) {
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
 
-      brightness = (pixels[(w * i) + j].red + pixels[(w * i) + j].blue + pixels[(w * i) + j].green)/3;
+      brightness = (pixels[(w * i) + j].red + pixels[(w * i) + j].blue + pixels[(w * i) + j].green) / 3;
 
-      if (brightness > 200) {
-          newPixels[(w * i) + j].red = brightness;
-          newPixels[(w * i) + j].blue = brightness; 
-          newPixels[(w * i) + j].green = brightness;
-      } else {
-        newPixels[(w * i) + j].red = 0;
-        newPixels[(w * i) + j].blue = 0; 
-        newPixels[(w * i) + j].green = 0; 
+      if (brightness > 200)
+      {
+        newPixels[(w * i) + j].red = brightness;
+        newPixels[(w * i) + j].blue = brightness;
+        newPixels[(w * i) + j].green = brightness;
       }
-      
+      else
+      {
+        newPixels[(w * i) + j].red = 0;
+        newPixels[(w * i) + j].blue = 0;
+        newPixels[(w * i) + j].green = 0;
+      }
     }
   }
 
   int blurVal = 0;
 
-  for (int i = 0; i < h; i++) {
-    for (int j = 0; j < w; j++) {
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
       blurVal = findAverage(newPixels, i, j, w, h);
-      blurred[(w*i) + j].blue = blurVal;
-      blurred[(w*i) + j].red =  blurVal;
-      blurred[(w*i) + j].green =  blurVal;
+      blurred[(w * i) + j].blue = blurVal;
+      blurred[(w * i) + j].red = blurVal;
+      blurred[(w * i) + j].green = blurVal;
     }
   }
 
   int index;
   int sum;
-  //write_ppm("check2.ppm", newPixels, w, h);
+  // write_ppm("check2.ppm", newPixels, w, h);
 
-  for (int i = 0; i < h; i++) {
-    for (int j = 0; j < w; j++) {
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
       index = (w * i) + j;
       sum = pixels[index].red + blurred[index].red;
 
-      if (sum > 255) {
-        newPixels[index].red = 255; 
-      } else {
+      if (sum > 255)
+      {
+        newPixels[index].red = 255;
+      }
+      else
+      {
         newPixels[index].red = sum;
       }
 
       sum = pixels[index].blue + blurred[index].blue;
 
-      if (sum > 255) {
-        newPixels[index].blue = 255; 
-      } else {
+      if (sum > 255)
+      {
+        newPixels[index].blue = 255;
+      }
+      else
+      {
         newPixels[index].blue = sum;
       }
 
       sum = pixels[index].green + blurred[index].green;
 
-      if (sum > 255) {
-        newPixels[index].green = 255; 
-      } else {
+      if (sum > 255)
+      {
+        newPixels[index].green = 255;
+      }
+      else
+      {
         newPixels[index].green = sum;
       }
-
     }
   }
 
   write_ppm("glow.ppm", newPixels, w, h);
-  //write_ppm("check.ppm", blurred, w, h);
-  
-  
+  // write_ppm("check.ppm", blurred, w, h);
+
   free(pixels);
   free(newPixels);
   free(blurred);
 
   return 0;
-  }
-
+}
--- A11/glow_multithreaded.c
+++ A11/glow_multithreaded.c
@@ -1,3 +1,8 @@
+/*Author: Olivia Boyer.
+ *Description: utilizes multiple threads for more efficient implementation of 
+ *glow filter. Takes command-line arguments to allow for various image inputs,
+ *blur radius, etc.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,186 +12,228 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 #include <pthread.h>
-//#include "ctype.h"
 
-struct threadArgs{
-  struct ppm_pixel* brightMap;
-  struct ppm_pixel* newPixels;
-  struct ppm_pixel* pixels;
+struct threadArgs
+{
+  struct ppm_pixel *brightMap;
+  struct ppm_pixel *newPixels;
+  struct ppm_pixel *pixels;
   int w;
   int h;
   int id;
-  pthread_barrier_t* barrier;
+  pthread_barrier_t *barrier;
   int blurSize;
   int threshold;
 };
 
-
-
-int findAverage(struct ppm_pixel* brightMap, int r, int c, 
-  int w, int h, int b) {
+int findAverage(struct ppm_pixel *brightMap, int r, int c,
+                int w, int h, int b)
+{
 
   //  printf("%d \n", r);
-  
-    int highW, lowW, highH, lowH;
-    int div = 0;
-    int sum = 0;
-    int dist = (b - 1)/2;
-  
-    if ((w - c) <= dist) {
-      highW = w;
-    } else { 
-      highW = c + dist;
-    }
-  
-    if ((h - r) <= dist) {
-      highH = h;
-    } else { 
-      highH = r + dist;
-    }
-  
-    if ((c) <= dist) {
-      lowW = 0;
-    } else { 
-      lowW = c - dist;
-    }
-  
-    if ((r) <= dist) {
-      lowH = 0;
-    } else { 
-      lowH = r - dist;
-    }
-  
-    for (int i = lowH; i < highH; i++) {
-      for (int j = lowW; j < highW; j++) {
-        sum += brightMap[(w * i) + j].red;
-        div += 1;
-      }
-    }
-    //printf("%d %d\n", r, c);
-  return sum/div;
+
+  int highW, lowW, highH, lowH;
+  int div = 0;
+  int sum = 0;
+  int dist = (b - 1) / 2;
+
+  if ((w - c) <= dist)
+  {
+    highW = w;
+  }
+  else
+  {
+    highW = c + dist;
   }
 
+  if ((h - r) <= dist)
+  {
+    highH = h;
+  }
+  else
+  {
+    highH = r + dist;
+  }
 
-void* glowEffect(void* arguments){
-  struct threadArgs* args = (struct threadArgs*) arguments;
+  if ((c) <= dist)
+  {
+    lowW = 0;
+  }
+  else
+  {
+    lowW = c - dist;
+  }
+
+  if ((r) <= dist)
+  {
+    lowH = 0;
+  }
+  else
+  {
+    lowH = r - dist;
+  }
+
+  for (int i = lowH; i < highH; i++)
+  {
+    for (int j = lowW; j < highW; j++)
+    {
+      sum += brightMap[(w * i) + j].red;
+      div += 1;
+    }
+  }
+  // printf("%d %d\n", r, c);
+  return sum / div;
+}
+
+void *glowEffect(void *arguments)
+{
+  struct threadArgs *args = (struct threadArgs *)arguments;
   int w = args->w;
   int h = args->h;
   int start = h * args->id;
   h = h + start;
   int brightness = 0;
 
-  for (int i = start; i < h; i++) {
-    for (int j = 0; j < args->w; j++) {
+  for (int i = start; i < h; i++)
+  {
+    for (int j = 0; j < args->w; j++)
+    {
 
-      brightness = (args->pixels[(w * i) + j].red + 
-        args->pixels[(w * i) + j].blue + args->pixels[(w * i) + j].green)/3;
+      brightness = (args->pixels[(w * i) + j].red +
+                    args->pixels[(w * i) + j].blue + args->pixels[(w * i) + j].green) /
+                   3;
 
-      if (brightness > args->threshold) {
-          args->newPixels[(w * i) + j].red = brightness;
-          args->newPixels[(w * i) + j].blue = brightness; 
-          args->newPixels[(w * i) + j].green = brightness;
-      } else {
+      if (brightness > args->threshold)
+      {
+        args->newPixels[(w * i) + j].red = brightness;
+        args->newPixels[(w * i) + j].blue = brightness;
+        args->newPixels[(w * i) + j].green = brightness;
+      }
+      else
+      {
         args->newPixels[(w * i) + j].red = 0;
-        args->newPixels[(w * i) + j].blue = 0; 
-        args->newPixels[(w * i) + j].green = 0; 
+        args->newPixels[(w * i) + j].blue = 0;
+        args->newPixels[(w * i) + j].green = 0;
       }
     }
   }
-      pthread_barrier_wait(args->barrier);
-
+  pthread_barrier_wait(args->barrier);
 
   int blurVal = 0;
 
-  for (int i = start; i < h; i++) {
-    for (int j = 0; j < w; j++) {
+  for (int i = start; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
       blurVal = findAverage(args->newPixels, i, j, w, h, args->blurSize);
-      args->brightMap[(w*i) + j].blue = blurVal;
-      args->brightMap[(w*i) + j].red =  blurVal;
-      args->brightMap[(w*i) + j].green =  blurVal;
+      args->brightMap[(w * i) + j].blue = blurVal;
+      args->brightMap[(w * i) + j].red = blurVal;
+      args->brightMap[(w * i) + j].green = blurVal;
     }
   }
 
-
   int index = 0;
-  int sum = 0; 
+  int sum = 0;
 
-  for (int i = start; i < h; i++) {
-    for (int j = 0; j < w; j++) {
+  for (int i = start; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
       index = (w * i) + j;
       sum = args->pixels[index].red + args->brightMap[index].red;
 
-      if (sum > 255) {
-        args->pixels[index].red = 255; 
-      } else {
+      if (sum > 255)
+      {
+        args->pixels[index].red = 255;
+      }
+      else
+      {
         args->pixels[index].red = sum;
       }
 
       sum = args->pixels[index].blue + args->brightMap[index].blue;
 
-      if (sum > 255) {
-        args->pixels[index].blue = 255; 
-      } else {
+      if (sum > 255)
+      {
+        args->pixels[index].blue = 255;
+      }
+      else
+      {
         args->pixels[index].blue = sum;
       }
 
       sum = args->pixels[index].green + args->brightMap[index].green;
 
-      if (sum > 255) {
-        args->pixels[index].green = 255; 
-      } else {
+      if (sum > 255)
+      {
+        args->pixels[index].green = 255;
+      }
+      else
+      {
         args->pixels[index].green = sum;
       }
-
     }
   }
   return NULL;
 }
 
-
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
   pthread_barrier_t barrier;
   int N = 4;
   int threshold = 200;
   int blursize = 24;
-  const char* filename = "earth-small.ppm";
+  const char *filename = "earth-small.ppm";
 
   int opt;
-  while ((opt = getopt(argc, argv, ":N:t:b:f:")) != -1) {
-    switch (opt) {
-      case 'N': N = atoi(optarg); break;
-      case 't': threshold = atof(optarg); break;
-      case 'b': blursize = atof(optarg); break;
-      case 'f': filename = optarg; break;
-      case '?': printf("usage: %s -N <NumThreads> -t <brightness threshold> -b <box blur size> -f <ppmfile>\n", argv[0]); break;
+  while ((opt = getopt(argc, argv, ":N:t:b:f:")) != -1)
+  {
+    switch (opt)
+    {
+    case 'N':
+      N = atoi(optarg);
+      break;
+    case 't':
+      threshold = atof(optarg);
+      break;
+    case 'b':
+      blursize = atof(optarg);
+      break;
+    case 'f':
+      filename = optarg;
+      break;
+    case '?':
+      printf("usage: %s -N <NumThreads> -t <brightness threshold> -b <box blur size> -f <ppmfile>\n", argv[0]);
+      break;
     }
   }
   pthread_barrier_init(&barrier, NULL, N);
   int w, h;
-  struct ppm_pixel* pixels = read_ppm(filename, &w, &h);
+  struct ppm_pixel *pixels = read_ppm(filename, &w, &h);
 
-  //printf("%d \n", h);
-  struct ppm_pixel* newPixels = (struct ppm_pixel*) malloc(
-    sizeof(struct ppm_pixel) * w * h);
+  // printf("%d \n", h);
+  struct ppm_pixel *newPixels = (struct ppm_pixel *)malloc(
+      sizeof(struct ppm_pixel) * w * h);
 
-  struct ppm_pixel* blurred = (struct ppm_pixel*) malloc(
-    sizeof(struct ppm_pixel) * w * h); 
+  struct ppm_pixel *blurred = (struct ppm_pixel *)malloc(
+      sizeof(struct ppm_pixel) * w * h);
 
-  if (pixels == NULL || newPixels == NULL || blurred == NULL) {
+  if (pixels == NULL || newPixels == NULL || blurred == NULL)
+  {
     printf("Error: failed to create array.");
     return 0;
   }
   pthread_barrier_init(&barrier, NULL, N);
 
-  pthread_t* tids = (pthread_t*) malloc(sizeof(pthread_t) * N);
+  pthread_t *tids = (pthread_t *)malloc(sizeof(pthread_t) * N);
 
-  struct threadArgs* threads = (struct threadArgs*)
-  malloc(sizeof(struct threadArgs) * N);
+  struct threadArgs *threads = (struct threadArgs *)
+      malloc(sizeof(struct threadArgs) * N);
 
   int chunk = h / N;
 
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
+  {
     threads[i].id = i;
     threads[i].w = w;
     threads[i].h = chunk;
@@ -196,16 +243,15 @@ int main(int argc, char* argv[])
     threads[i].barrier = &barrier;
     threads[i].blurSize = blursize;
     threads[i].threshold = threshold;
-    pthread_create(&tids[i], NULL, glowEffect, (void*) &threads[i]);
+    pthread_create(&tids[i], NULL, glowEffect, (void *)&threads[i]);
   }
 
-
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
+  {
     pthread_join(tids[i], NULL);
   }
-
-  write_ppm("check.ppm", newPixels, w, h);
-  write_ppm("multiglow.ppm", pixels, w, h);
+ 
+  write_ppm("glow.ppm", pixels, w, h);
 
   free(threads);
   free(pixels);
@@ -213,6 +259,5 @@ int main(int argc, char* argv[])
   free(blurred);
   free(tids);
   return 0;
-  // todo: your code here
-  // compute image
+
 }
