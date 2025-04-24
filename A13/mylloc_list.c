ee75db3a6fb0d45f3f8d40cdfb6694a4290253ff
Olivia Brown Boyer <oboyer@brynmawr.edu>
2025-04-23 23:43:52 -0400

    finished A13

--- A13/mylloc_list.c
+++ A13/mylloc_list.c
@@ -1,20 +1,70 @@
+/*Name: Olivia Boyer (though mostly based on Montelius malloc text)
+*Desc: implements a simple memory allocator.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-struct chunk {
+struct chunk
+{
   int size;
   int used;
   struct chunk *next;
 };
 struct chunk *flist = NULL;
 
-void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
+void *malloc(size_t size)
+{
+  if (size == 0)
+  {
+    return NULL;
+  }
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  while (next != NULL)
+  {
+    if (next->size >= size)
+    {
+      if (prev != NULL)
+      {
+        prev->next = next->next;
+      }
+      else
+      {
+        flist = next->next;
+      }
+      next->used = size;
+      return (void *)(next + 1);
+    }
+    else
+    {
+      prev = next;
+      next = next->next;
+    }
+  }
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void *)-1)
+  {
+    return NULL;
+  }
+  else
+  {
+    struct chunk *cnk = (struct chunk *)memory;
+    cnk->used = size;
+    cnk->size = size;
+    return (void *)(cnk + 1);
+  }
+
   return NULL;
 }
 
-void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
+void free(void *memory)
+{
+  struct chunk *cnk = (struct chunk *)((struct chunk *)memory - 1);
+  cnk->next = flist;
+  flist = cnk;
+
   return;
 }
